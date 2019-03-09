25ab6ce56bfcb7e436b7fb42aadc49cb8bf1bee8
olexy <h.lekan@gmail.com>
2019-03-09 17:40:46 +0100

    model & migration

---
+++ database/migrations/2019_03_09_111612_create_transfers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransfersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transfers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('fullname');
+            $table->string('description');
+            $table->string('accountNumber');
+            $table->string('bankName');
+            $table->string('recipientCode');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transfers');
+    }
+}
